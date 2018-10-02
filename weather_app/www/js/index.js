4b6610034dc4015836c72a447d82945ec6e1bb9e
Martín Isusi Seff <martinisusi@MacBook-Air.local>
2018-10-01 21:07:03 -0300

    Adding time and commenting animations

--- weather_app/www/js/index.js
+++ weather_app/www/js/index.js
@@ -9,3 +9,25 @@ function addOutlines() {
         outlines = false;
     }
 }
+
+(function (){
+    //var myWorker = new Worker("js/clock.js");
+    document.onload = startTime();
+})();
+
+function startTime() {
+    /*var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;*/
+    var d = new Date();
+    var timeString = (d.getHours()<10?"0"+d.getHours():d.getHours()) + ":" + (d.getMinutes()<10?"0"+d.getMinutes():d.getMinutes()) + ":" + (d.getSeconds()<10?"0"+d.getSeconds():d.getSeconds()) + " ";
+    $('#time').html(timeString);
+    $('#date').html(d.toLocaleDateString("es-AR"));
+    console.log(timeString);
+    var t = setTimeout(startTime, 500);
+}
